7895009d0f3f7005e2b3b425ab0cc4c747156941
Bryan Novak <bjnovak125@gmail.com>
2024-02-15 20:27:03 -0600

    Update TransactionsData.java

--- src/main/java/com/problem2/demo/service/TransactionsData.java
+++ src/main/java/com/problem2/demo/service/TransactionsData.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -14,6 +15,12 @@ import org.springframework.core.io.Resource;
 import com.problem2.demo.model.Transaction;
 
 public class TransactionsData {
+
+    private static int getPoints(int amount) {
+        if (amount <= 50) return 0;
+        else if (amount > 50 && amount <= 100) return amount - 50;
+        else return ((amount - 100) * 2) + 50;
+    }
 
     public static ArrayList<Transaction> transactions() {
         try {
@@ -31,10 +38,7 @@ public class TransactionsData {
                     data.path("customerId").asInt(),
                     data.path("amount").asInt(),
                     data.path("points").asInt(),
-                    Date.from(LocalDateTime.ofInstant(
-                        Instant.parse(data.path("date").asText()), ZoneId.of("UTC"))
-                        .atZone(ZoneId.systemDefault()).toInstant()
-                    )
+                    data.path("date").asText()
                 ));
             }
 
@@ -46,55 +50,53 @@ public class TransactionsData {
         return null;
     }
 
-    public static Map<String, Integer> totals() {
-        try {
-            Resource resource = new ClassPathResource("data.json");
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(resource.getInputStream());
-            Map<String, Integer> total = new HashMap<>();
-            JsonNode totalsByMonth = jsonNode.path("totals");
+    public static Map<Integer, Integer> totals() {
+        ArrayList<Transaction> transactions = TransactionsData.transactions();
 
-            for (Iterator<String> it = totalsByMonth.fieldNames(); it.hasNext(); ) {
-                String customerId = it.next();
-                JsonNode monthData = totalsByMonth.path(customerId);
-                total.put(customerId, Integer.parseInt(monthData.toString()));
+        Map<Integer, Integer> customerTotals = new HashMap<>();
+
+        for (Transaction tx : Objects.requireNonNull(transactions)) {
+            final int customerId = tx.customerId();
+            if (customerTotals.containsKey(customerId)) {
+                customerTotals.put(customerId, customerTotals.get(customerId) + getPoints(tx.amount()));
+            } else {
+                customerTotals.put(customerId, getPoints(tx.amount()));
             }
-
-            return total;
-        } catch (Exception e) {
-            e.printStackTrace();
         }
 
-        return null;
+        return customerTotals;
     }
 
-    public static Map<String, Map<String, Integer>> totalsByMonth() {
-        try {
-            Resource resource = new ClassPathResource("data.json");
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(resource.getInputStream());
-            Map<String, Map<String, Integer>> totals = new HashMap<>();
-            JsonNode totalsByMonth = jsonNode.path("totalsByMonth");
+    public static Map<Integer, Map<String, Integer>> totalsByMonth() {
+        ArrayList<Transaction> transactions = TransactionsData.transactions();
 
-            for (Iterator<String> it = totalsByMonth.fieldNames(); it.hasNext(); ) {
-                String customerId = it.next();
-                Map<String, Integer> monthTotals = new HashMap<>();
-                JsonNode monthData = totalsByMonth.path(customerId);
+        Map<Integer, Map<String, Integer>> totalsByMonth = new HashMap<>();
 
-                for (Iterator<String> monthIt = monthData.fieldNames(); monthIt.hasNext(); ) {
-                    String month = monthIt.next();
-                    monthTotals.put(month, Integer.parseInt(monthData.path(month).toString()));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-yyyy");
+
+        for (Transaction tx : Objects.requireNonNull(transactions)) {
+            Instant instant = Instant.parse(tx.date());
+            LocalDateTime dateTime = LocalDateTime.ofInstant(instant, ZoneId.of("UTC"));
+            String monthId = dateTime.format(formatter);
+
+            final int customerId = tx.customerId();
+
+            if (totalsByMonth.containsKey(customerId) && totalsByMonth.get(customerId).containsKey(monthId)) {
+                totalsByMonth.get(customerId).put(monthId,
+                        totalsByMonth.get(customerId).get(monthId) + getPoints(tx.amount())
+                );
+            } else {
+                if (!totalsByMonth.containsKey(customerId)) {
+                    Map<String, Integer> newMonth = new HashMap<>();
+                    newMonth.put(monthId, getPoints(tx.amount()));
+                    totalsByMonth.put(customerId, newMonth);
+                } else {
+                    totalsByMonth.get(customerId).put(monthId, getPoints(tx.amount()));
                 }
-
-                totals.put(customerId, monthTotals);
             }
-
-            return totals;
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
-        return null;
+        return totalsByMonth;
     }
 
 }
